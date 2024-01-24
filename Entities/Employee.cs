4d94c547b532eb774dffd88600f9b532a9b38b6e
lexit <110407145+iTzlexit@users.noreply.github.com>
2024-01-24 16:36:44 +0200

    Add project files.

---
+++ Entities/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace Entities
+{
+    public class Employee
+    {
+        public uint empId; // non negative int 
+        public string empName;
+        public double salaryPerHour;
+        public double noOfWorkingHours;
+        public double salary;
+
+
+        public static string organizationName; // initialize it in the main method 
+
+
+        public const string typeOfEmployee = "Contract Based";
+        public readonly string departmentName;
+
+        //constructor: executes every time when we create an object for the Employee class.
+        public Employee()
+        {
+            departmentName = "Finance Department"; 
+        }
+
+    }
+}
---
+++ MultipleObjectsAssignment/Program.cs
@@ -0,0 +1,78 @@
+﻿using Entities;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsumesEmployees
+{
+    public class ConsumesEmployeesAssignment
+    {
+        public static void Main()
+        {
+            // initializing the static field. 
+            Employee.organizationName = "Harsha Inc";
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                string employeeNumber;
+
+                switch (i)
+                {
+                    case 0: employeeNumber = "1st Employee:"; break;
+                    case 1: employeeNumber = "2nd Employee:"; break;
+                    case 2: employeeNumber = "3rd Employee:"; break;
+                    case 3: employeeNumber = "4th Employee:"; break;
+                    case 4: employeeNumber = "5th Employee:"; break;
+                    default: employeeNumber = "Other employee"; break;
+
+                }
+
+                Console.WriteLine(employeeNumber);
+                // create Employee Object 
+
+                Employee employee = new Employee();
+                // enter employee id 
+                Console.Write("Enter the employees Id");
+                employee.empId = uint.Parse(Console.ReadLine()); // from string to unit 
+
+                // enter employees name 
+                Console.Write("Enter employees name: ");
+                employee.empName = Console.ReadLine();
+
+                // enter employee salary per hour 
+                Console.Write("Salary Per Hour :");
+                employee.salaryPerHour = uint.Parse(Console.ReadLine());
+
+                // no of working hours 
+
+                Console.Write("Number of working hours");
+                employee.noOfWorkingHours = uint.Parse(Console.ReadLine());
+
+                //calc the salary
+                employee.salary = employee.noOfWorkingHours * employee.salaryPerHour;
+
+                //display Employee Details
+                System.Console.WriteLine("\nDETAILS OF " + employeeNumber);
+                System.Console.WriteLine("Employee ID: " + employee.empId);
+                System.Console.WriteLine("Employee Name: " + employee.empName);
+                System.Console.WriteLine("Salary per Hour: " + employee.salaryPerHour);
+                System.Console.WriteLine("No. of Working Hours: " + employee.noOfWorkingHours);
+                System.Console.WriteLine("Net Salary: " + employee.salary);
+                System.Console.WriteLine("Type of Employee: " + Employee.typeOfEmployee);
+                System.Console.WriteLine("Department Name: " + employee.departmentName);
+
+                Console.WriteLine();
+
+            }
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
